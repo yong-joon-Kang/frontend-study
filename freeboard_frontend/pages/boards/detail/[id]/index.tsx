2a8e4b87b0b8f0d25be151e461fbf39b5a81608f
DESKTOP-3GEP8UR\kangy <kangyj2197@gmail.com>
2023-04-01 01:40:08 +0900

    [MODIFY] detail index.tsx

--- freeboard_frontend/pages/boards/detail/[id]/index.tsx
+++ freeboard_frontend/pages/boards/detail/[id]/index.tsx
@@ -1,25 +1,10 @@
 import BoardDetail from "../../../../src/components/board/detail/BoardDetail.container";
 import BoardCommentWrite from "../../../../src/components/boardComment/write/BoardCommentWrite.container";
 import BoardCommentList from "../../../../src/components/boardComment/list/BoardCommentList.container";
-import { useRouter } from "next/router";
-import { message } from "antd";
 
 export default function detailPage() {
-  const router = useRouter();
-  const [messageApi, contextHolder] = message.useMessage(); // alert Message
-  console.log(router.query.crud);
-
-  if (router.query.crud === "create") {
-    console.log("gooood");
-    messageApi.open({
-      type: "success",
-      content: "success",
-    });
-  }
-
   return (
     <>
-      {contextHolder}
       <BoardDetail />
       <BoardCommentWrite />
       <BoardCommentList />
