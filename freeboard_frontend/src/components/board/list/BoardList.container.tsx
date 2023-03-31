d8e460a2371d5acabf9bad966601058631a10e13
DESKTOP-3GEP8UR\kangy <kangyj2197@gmail.com>
2023-04-01 01:50:21 +0900

    [ADD]  Pagination

--- freeboard_frontend/src/components/board/list/BoardList.container.tsx
+++ freeboard_frontend/src/components/board/list/BoardList.container.tsx
@@ -1,18 +1,33 @@
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
 import BoardListPresenterPage from "./BoardList.presenter";
-import { FETCH_BOARDS } from "./BoardList.queries";
+import { FETCH_BOARDS, FETCH_BOARDS_COUNT } from "./BoardList.queries";
 import { useQuery } from "@apollo/client";
 import router from "next/router";
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 
 import {
   IQuery,
   IQueryFetchBoardsArgs,
+  IQueryFetchBoardsCountArgs,
 } from "../../../commons/types/generated/types";
+import styled from "@emotion/styled";
 
 export default function BoardListContainerPage() {
-  const { data } = useQuery<Pick<IQuery, "fetchBoards">, IQueryFetchBoardsArgs>(
-    FETCH_BOARDS
-  );
+  const [startPage, setStartPage] = useState(1);
+
+  const { data: fetchBoardsCount } = useQuery<
+    Pick<IQuery, "fetchBoardsCount">,
+    IQueryFetchBoardsCountArgs
+  >(FETCH_BOARDS_COUNT);
+
+  const lastPageNum = fetchBoardsCount
+    ? Math.ceil(fetchBoardsCount.fetchBoardsCount / 10)
+    : 0;
+
+  const { data, refetch } = useQuery<
+    Pick<IQuery, "fetchBoards">,
+    IQueryFetchBoardsArgs
+  >(FETCH_BOARDS);
 
   const onClickBoardWrite = () => {
     router.push("/boards/new");
@@ -22,11 +37,44 @@ export default function BoardListContainerPage() {
     router.push(`/boards/detail/${event.currentTarget.id}`);
   };
 
+  const onClickPage = (event: MouseEvent<HTMLSpanElement>) => {
+    refetch({ page: Number(event.currentTarget.id) });
+  };
+
+  const onClickPrev = () => {
+    if (startPage === 1) return;
+    setStartPage((prev) => prev - 10);
+  };
+
+  const onClickNext = () => {
+    if (startPage + 10 <= lastPageNum) setStartPage((prev) => prev + 10);
+  };
+
+  const PageNum = styled.span`
+    margin: 10px;
+  `;
+
   return (
-    <BoardListPresenterPage
-      data={data}
-      onClickBoardWrite={onClickBoardWrite}
-      onClickBoardDetail={onClickBoardDetail}
-    />
+    <>
+      <BoardListPresenterPage
+        data={data}
+        onClickBoardWrite={onClickBoardWrite}
+        onClickBoardDetail={onClickBoardDetail}
+      />
+      <span onClick={onClickPrev}>이전</span>
+      {new Array(10).fill(1).map(
+        (el, index) =>
+          index + startPage <= lastPageNum && (
+            <PageNum
+              key={index + startPage}
+              id={String(index + startPage)}
+              onClick={onClickPage}
+            >
+              {index + startPage}
+            </PageNum>
+          )
+      )}
+      <span onClick={onClickNext}>다음</span>
+    </>
   );
 }
--- freeboard_frontend/src/components/board/list/BoardList.queries.ts
+++ freeboard_frontend/src/components/board/list/BoardList.queries.ts
@@ -1,12 +1,18 @@
 import { gql } from "@apollo/client";
 
 export const FETCH_BOARDS = gql`
-  query {
-    fetchBoards(page: 1) {
+  query fetchBoards($page: Int) {
+    fetchBoards(page: $page) {
       writer
       title
       createdAt
       _id
     }
+  }
+`;
+
+export const FETCH_BOARDS_COUNT = gql`
+  query fetchBoardsCount {
+    fetchBoardsCount
   }
 `;
