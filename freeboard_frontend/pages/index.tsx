9f57ca5f0acd747d98e44a4ef0e178f572872854
DESKTOP-3GEP8UR\kangy <kangyj2197@gmail.com>
2023-04-25 23:28:06 +0900

    [ADD] 로그인 완료 시 메인페이지

--- freeboard_frontend/pages/index.tsx
+++ freeboard_frontend/pages/index.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <></>;
+import { useQuery } from "@apollo/client";
+import { FETCH_USER_LOGGED_IN } from "../src/components/signIn/SignIn.queries";
+import { userNameState } from "../src/commons/libraries/recoil";
+import { IQuery } from "../src/commons/types/generated/types";
+import { useRecoilState } from "recoil";
+import { useEffect } from "react";
+
+function Home() {
+  const [, setUserName] = useRecoilState(userNameState);
+  const { data } =
+    useQuery<Pick<IQuery, "fetchUserLoggedIn">>(FETCH_USER_LOGGED_IN);
+  if (data) setUserName(data?.fetchUserLoggedIn.name);
+
+  useEffect(() => {
+    setUserName(data?.fetchUserLoggedIn.name ?? "");
+    localStorage.setItem("userName", data?.fetchUserLoggedIn.name ?? "");
+  }, []);
+
+  return <div>Home</div>;
 }
+
+export default Home;
