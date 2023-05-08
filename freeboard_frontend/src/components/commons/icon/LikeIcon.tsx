9ab9473a87e01113259dc303d5a0ced0cb5ce42f
DESKTOP-3GEP8UR\kangy <kangyj2197@gmail.com>
2023-05-08 23:33:40 +0900

    [ADD] LikeIcon Component 공통으로 분리

---
+++ freeboard_frontend/src/components/commons/icon/LikeIcon.tsx
@@ -0,0 +1,12 @@
+import { HeartFilled } from "@ant-design/icons";
+import styled from "@emotion/styled";
+
+const Like = styled(HeartFilled)`
+  color: #ffd600;
+`;
+
+function LikeIcon() {
+  return <Like />;
+}
+
+export default LikeIcon;
