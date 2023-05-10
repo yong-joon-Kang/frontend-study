20616cc937b4a2ec92e55c74ac9ca563d4da2357
DESKTOP-3GEP8UR\kangy <kangyj2197@gmail.com>
2023-05-10 23:54:58 +0900

    [ADD] common css 추가

---
+++ freeboard_frontend/styles/common.ts
@@ -0,0 +1,17 @@
+export const color = {
+  primary: "#ffd600",
+};
+
+export const mideaQuery = {
+  width1300: "1300px",
+  width1100: "1100px",
+  width1000: "1000px",
+  width900: "900px",
+  width800: "800px",
+  width700: "700px",
+  width650: "650px",
+  width620: "620px",
+  width410: "410px",
+  width600: "600px",
+  width500: "500px",
+};
