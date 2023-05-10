16884905830498428b28ac6430e3048822479e77
DESKTOP-3GEP8UR\kangy <kangyj2197@gmail.com>
2023-05-10 23:41:49 +0900

    [MODIFY] banner style 수정

--- freeboard_frontend/src/components/layout/banner/index.tsx
+++ freeboard_frontend/src/components/layout/banner/index.tsx
@@ -1,6 +1,4 @@
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import styled from "@emotion/styled";
 
 const BannerWrap = styled.div`
@@ -22,17 +20,71 @@ const Banner = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false,
+    arrows: true,
+    appendDots: (dots: any) => <SlickDots>{dots}</SlickDots>,
   };
+
+  const SlickDots = styled.ul`
+    // slick-dots 스타일 변경
+    width: auto;
+    bottom: 6px;
+    background-color: rgba(0, 0, 0, 0.3);
+    border-radius: 20px;
+    display: flex;
+    height: 25px;
+    align-items: center;
+
+    .slick-prev {
+      /* 이전 버튼 스타일 변경 */
+      left: 20px !important;
+      z-index: 1 !important;
+    }
+
+    button:before {
+      color: #fff !important;
+    }
+
+    .slick-active {
+      // slick-dots 내부 active 스타일 변경
+      color: #fff;
+    }
+    li.slick-active button:before {
+      color: #fff;
+    }
+    li.slick-active button {
+      color: #fff;
+    }
+  `;
+
+  const SlickSlider = styled(Slider)`
+    /* slick-slider 스타일 변경 */
+    display: flex;
+    justify-content: center;
+
+    .slick-arrow {
+      &.slick-prev {
+        left: 20px;
+        z-index: 1;
+      }
+    }
+
+    .slick-arrow {
+      &.slick-next {
+        right: 20px;
+        z-index: 1;
+      }
+    }
+  `;
+
   return (
     <BannerWrap>
-      <Slider {...settings}>
+      <SlickSlider {...settings}>
         {Array(3)
           .fill("/bannerImage.jpg")
           .map((el, index) => (
             <Img key={index} src={el} />
           ))}
-      </Slider>
+      </SlickSlider>
     </BannerWrap>
   );
 };
