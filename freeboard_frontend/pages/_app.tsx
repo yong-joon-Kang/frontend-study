45957fee7441d9bd14052c90d54e663f613130d4
DESKTOP-3GEP8UR\kangy <kangyj2197@gmail.com>
2023-05-10 23:41:25 +0900

    [MODIFY] slick css 파일 이동, app.tsx에서 import

--- freeboard_frontend/pages/_app.tsx
+++ freeboard_frontend/pages/_app.tsx
@@ -5,30 +5,32 @@ import "../styles/myGlobals.css";
 
 import { RecoilRoot } from "recoil";
 
-import { createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
-import logger from "redux-logger";
-import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "../src/commons/libraries/reducers";
+// import { createStore, applyMiddleware, compose } from "redux";
+// import { Provider } from "react-redux";
+// import logger from "redux-logger";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import rootReducer from "../src/commons/libraries/reducers";
+import "../styles/slick-theme.css";
+import "../styles/slick.css";
 
 // 배포 레벨에서는 리덕스 발동시 찍히는 logger를 사용하지 않습니다.
-const enhancer =
-  process.env.NODE_ENV === "production"
-    ? compose(applyMiddleware())
-    : composeWithDevTools(applyMiddleware(logger));
+// const enhancer =
+//   process.env.NODE_ENV === "production"
+//     ? compose(applyMiddleware())
+//     : composeWithDevTools(applyMiddleware(logger));
 
 // 위에서 만든 reducer를 스토어 만들때 넣어줍니다
-const store = createStore(rootReducer, enhancer);
+// const store = createStore(rootReducer, enhancer);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <ApolloSettings>
-        <Provider store={store}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </Provider>
+        {/* <Provider store={store}> */}
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        {/* </Provider> */}
       </ApolloSettings>
     </RecoilRoot>
   );
