adf03f615bc99f08e1581289ac68ecb2cc7cc8ce
DESKTOP-3GEP8UR\kangy <kangyj2197@gmail.com>
2023-05-13 00:54:41 +0900

    [ADD] AnswerArrow Component 공통으로 분리

---
+++ freeboard_frontend/src/components/commons/icon/AnswerArrow.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const Arrow = styled.img`
+  height: 30px;
+  margin-left: 60px;
+`;
+
+function AnswerArrow() {
+  return <Arrow src="/answerArrow.png" />;
+}
+
+export default AnswerArrow;
