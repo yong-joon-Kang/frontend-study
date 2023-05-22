d3de6ecafc1bb34ab281ef317c1262dc0d5d9f72
DESKTOP-3GEP8UR\kangy <kangyj2197@gmail.com>
2023-05-22 17:23:13 +0900

    [MODIFY] NavMenu 프로필 사진 정상표출, 클릭 시 myPage 이동

--- freeboard_frontend/src/components/layout/NavMenu/index.tsx
+++ freeboard_frontend/src/components/layout/NavMenu/index.tsx
@@ -1,4 +1,7 @@
 import styled from "@emotion/styled";
+import { useRecoilState } from "recoil";
+import { userInfoState } from "../../../commons/libraries/recoil";
+import { useRouter } from "next/router";
 
 const Wrap = styled.div`
   background-color: #fff;
@@ -24,12 +27,22 @@ const Li = styled.div`
   cursor: pointer;
 `;
 
-const Profile = styled.div`
-  border: 1px solid;
-  border-radius: 100px;
-  height: 150px;
+export const ProfileImg = styled.img`
+  width: 120px;
+  height: 120px;
+`;
+
+export const ProfileRadius = styled.div`
   width: 150px;
+  height: 150px;
+  border: 1px solid #bbb;
+  border-radius: 50%;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   margin: 150px 0 80px 0;
+  cursor: pointer;
 `;
 
 interface ILayoutProps {
@@ -38,9 +51,24 @@ interface ILayoutProps {
 
 // eslint-disable-next-line react/display-name
 const NavMenu = (props: ILayoutProps) => {
+  const router = useRouter();
+  const [userInfo] = useRecoilState(userInfoState);
+
   return (
     <Wrap>
-      <Profile></Profile>
+      <ProfileRadius
+        onClick={() => {
+          router.push("/myPage");
+        }}
+      >
+        <ProfileImg
+          src={
+            userInfo.picture
+              ? `https://storage.googleapis.com/${userInfo.picture}`
+              : "/profileDef.png"
+          }
+        />
+      </ProfileRadius>
       <NavigateWrap>
         <Ul>
           <Li
