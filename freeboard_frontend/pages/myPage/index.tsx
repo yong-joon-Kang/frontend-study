7a189da681a2688f7b07d530305dde1045545dcd
DESKTOP-3GEP8UR\kangy <kangyj2197@gmail.com>
2023-05-22 17:14:16 +0900

    [ADD] myPage 메뉴별 클릭 컴포넌트

--- freeboard_frontend/pages/myPage/index.tsx
+++ freeboard_frontend/pages/myPage/index.tsx
@@ -1,7 +1,31 @@
-import MyPageContainer from "../../src/components/myPage/myPage.container";
+import { useState } from "react";
+import LeftMenu from "../../src/components/myPage/leftMenu/LeftMenu";
+import styled from "@emotion/styled";
+import MyCartPageContainer from "../../src/components/myPage/myCartPage/MyCartPage.container";
+import MyPointContainer from "../../src/components/myPage/myPoint/MyPoint.container";
+import MyProfileContainer from "../../src/components/myPage/MyProfile/MyProfile.container";
+import MySettingsContainer from "../../src/components/myPage/mySettings/mySettings.container";
+
+export const Wrap = styled.div`
+  display: flex;
+`;
 
 function index() {
-  return <MyPageContainer />;
+  const [currPage, setCurrPage] = useState("myCartPage");
+
+  const onClickMenu = (pageNm: string) => {
+    setCurrPage(pageNm);
+  };
+  return (
+    <Wrap>
+      <LeftMenu onClickMenu={onClickMenu} />
+      {/* <MyBoard></MyBoard> */}
+      {currPage === "myCartPage" && <MyCartPageContainer />}
+      {currPage === "myPoint" && <MyPointContainer />}
+      {currPage === "myProfile" && <MyProfileContainer />}
+      {currPage === "mySettings" && <MySettingsContainer />}
+    </Wrap>
+  );
 }
 
 export default index;
