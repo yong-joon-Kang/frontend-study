d3d75e47530182d2cd9c1a32b145a138c972ceb6
DESKTOP-3GEP8UR\kangy <kangyj2197@gmail.com>
2023-05-27 21:48:25 +0900

    [ADD] 자유게시판 - detailPage youTube 동영상 기능

--- freeboard_frontend/src/components/board/detail/BoardDetail.presenter.tsx
+++ freeboard_frontend/src/components/board/detail/BoardDetail.presenter.tsx
@@ -2,10 +2,31 @@ import * as S from "./BoardDetail.styles";
 import { getDate } from "../../../commons/libraries/utils";
 import { IBoardDetailPresenterPageProps } from "./BoardDetail.types";
 import { Tooltip } from "antd";
+import SlickSlide from "../../commons/slickSlide/SlickSlide";
+import YouTube from "react-youtube";
 export default function BoardDetailPresenterPage(
   props: IBoardDetailPresenterPageProps
 ) {
   const fetchBoard = props.data?.fetchBoard;
+  let videoId;
+  if (fetchBoard?.youtubeUrl) {
+    const regExp =
+      /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?(?:.+&)?v=|shorts\/)|youtu\.be\/)([^#&?]+)/;
+    const match = fetchBoard?.youtubeUrl?.match(regExp);
+
+    if (match?.[1]) {
+      videoId = match[1];
+    } else {
+      videoId = null;
+    }
+  }
+  console.log(videoId);
+
+  // let youtubeUrl = fetchBoard?.youtubeUrl;
+  // if(youtubeUrl?.includes("shorts")){
+
+  // }
+  // const youtubeUrl = fetchBoard?.youtubeUrl
   return (
     <S.Wrapper>
       <S.CardWrapper>
@@ -32,13 +53,24 @@ export default function BoardDetailPresenterPage(
         </S.Header>
         <S.Contents>
           <S.Title>{fetchBoard?.title}</S.Title>
-          {fetchBoard?.contents}
-          {fetchBoard?.images?.map((el, index) => (
-            <S.UploadImg
-              key={index}
-              src={`https://storage.googleapis.com/${el}`}
-            ></S.UploadImg>
-          ))}
+          <S.Content>{fetchBoard?.contents}</S.Content>
+          <SlickSlide images={fetchBoard?.images ?? []} />
+          <YouTube
+            style={{ margin: "200px auto 0 auto" }}
+            videoId={videoId ?? ""} // defaults -> null
+            opts={{
+              width: "560",
+              height: "315",
+              playerVars: {
+                autoplay: 0, // 자동재생 O
+                rel: 0, // 관련 동영상 표시하지 않음
+                modestbranding: 1, // 컨트롤 바에 youtube 로고를 표시하지 않음
+              },
+            }}
+            onEnd={(e) => {
+              e.target.stopVideo(0);
+            }}
+          />
         </S.Contents>
         <S.Footer>
           <S.LikeWrap isLikeIcon={true} onClick={props.onClickCountLike}>
