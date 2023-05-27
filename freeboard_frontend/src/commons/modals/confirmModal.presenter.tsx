8d1ef03dfeaf064ad5174024e17df43897c99312
DESKTOP-3GEP8UR\kangy <kangyj2197@gmail.com>
2023-05-27 21:45:28 +0900

    [ADD] 포인트 충전 시 Modal

--- freeboard_frontend/src/commons/modals/confirmModal.presenter.tsx
+++ freeboard_frontend/src/commons/modals/confirmModal.presenter.tsx
@@ -1,4 +1,6 @@
 import { Modal } from "antd";
+import * as S from "./confirmModal.styles";
+import Select from "react-select";
 
 const ConfirmModalPresenter = (props: any) => {
   return (
@@ -7,7 +9,25 @@ const ConfirmModalPresenter = (props: any) => {
         open={props.ismodalToggle}
         onOk={props.handleOk}
         onCancel={props.onToggleModal}
+        footer={false}
+        width={props.btnFnc === "charging" ? 350 : 400}
       >
+        {props.btnFnc === "charging" && (
+          <>
+            <S.Header>
+              <S.Img src="/charging.png" />
+              <div>
+                <strong>충전하실 금액을 선택해주세요!</strong>
+              </div>
+            </S.Header>
+            <Select
+              defaultValue={props.selectedOption}
+              onChange={props.setSelectedOption}
+              options={props.options}
+            />
+          </>
+        )}
+
         {props.btnFnc === "boardDelete" && <p>정말로 삭제하시겠습니까?</p>}
         {props.btnFnc === "usedItemQuestionDelete" && (
           <p>정말로 삭제하시겠습니까?</p>
@@ -28,6 +48,24 @@ const ConfirmModalPresenter = (props: any) => {
             <p>비밀번호를 입력하세요.</p>
           </>
         )}
+        <S.Footer>
+          <S.Button
+            btnFnc={props.btnFnc}
+            type="primary"
+            onClick={props.handleOk}
+          >
+            {props.btnFnc === "charging" ? (
+              <span>충전하기</span>
+            ) : (
+              <span>확인</span>
+            )}
+          </S.Button>
+          {props.btnFnc !== "charging" && (
+            <S.Button isCancel={true} onClick={props.onToggleModal}>
+              취소
+            </S.Button>
+          )}
+        </S.Footer>
       </Modal>
     </>
   );
