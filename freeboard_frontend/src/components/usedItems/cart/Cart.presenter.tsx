34558dc87f58d9e49bcff8d7d5bf82c77e6ef4e9
DESKTOP-3GEP8UR\kangy <kangyj2197@gmail.com>
2023-07-04 20:39:45 +0900

    [MODIFY] build 에러 잡기 위한 테스트

--- freeboard_frontend/src/components/usedItems/cart/Cart.presenter.tsx
+++ freeboard_frontend/src/components/usedItems/cart/Cart.presenter.tsx
@@ -1,6 +1,7 @@
+import { getComma } from "../../../commons/libraries/utils";
 import * as S from "./Cart.styles";
 import { IProps } from "./Cart.types";
-import OneItem from "./oneItem";
+// import OneItem from "./oneItem";
 
 function CartPresenter(props: IProps) {
   return (
@@ -21,56 +22,56 @@ function CartPresenter(props: IProps) {
 
       {JSON.stringify(props?.cartData) !== "[{}]" &&
         props?.cartData.map((el: any, index: number) => (
-          <OneItem
-            key={index}
-            el={el}
-            index={index}
-            onChangeOne={props.onChangeOne}
-            onClickQuantityBtn={props.onClickQuantityBtn}
-            onChangeInput={props.onChangeInput}
-          />
-          // <S.ItemWrap key={el._id}>
-          //   <S.CheckBox
-          //     onChange={() => {
-          //       props.onChangeOne(index);
-          //     }}
-          //     style={{ width: "30px", height: "30px" }}
-          //     type="checkbox"
-          //     checked={el.isChecked}
-          //   />
-          //   <S.RightWrap>
-          //     <S.Img src={`https://storage.googleapis.com/${el.images?.[0]}`} />
-          //   </S.RightWrap>
-          //   <S.MiddleWrap>
-          //     <S.Name>{el.name}</S.Name>
-          //     <S.Price>{getComma(String(el.oneItemTotalPrice))}원</S.Price>
-          //   </S.MiddleWrap>
-          //   <S.LeftWrap>
-          //     <S.Quantity>
-          //       <S.QuantityBtn
-          //         onClick={() => {
-          //           props.onClickQuantityBtn(index, "minus");
-          //         }}
-          //       >
-          //         -
-          //       </S.QuantityBtn>
-          //       <S.Input
-          //         value={Number(el.quantity)}
-          //         maxLength={3}
-          //         onChange={() => {
-          //           props.onChangeInput(event, index);
-          //         }}
-          //       />
-          //       <S.QuantityBtn
-          //         onClick={() => {
-          //           props.onClickQuantityBtn(index, "plus");
-          //         }}
-          //       >
-          //         +
-          //       </S.QuantityBtn>
-          //     </S.Quantity>
-          //   </S.LeftWrap>
-          // </S.ItemWrap>
+          // <OneItem
+          //   key={index}
+          //   el={el}
+          //   index={index}
+          //   onChangeOne={props.onChangeOne}
+          //   onClickQuantityBtn={props.onClickQuantityBtn}
+          //   onChangeInput={props.onChangeInput}
+          // />
+          <S.ItemWrap key={el._id}>
+            <S.CheckBox
+              onChange={() => {
+                props.onChangeOne(index);
+              }}
+              style={{ width: "30px", height: "30px" }}
+              type="checkbox"
+              checked={el.isChecked}
+            />
+            <S.RightWrap>
+              <S.Img src={`https://storage.googleapis.com/${el.images?.[0]}`} />
+            </S.RightWrap>
+            <S.MiddleWrap>
+              <S.Name>{el.name}</S.Name>
+              <S.Price>{getComma(String(el.oneItemTotalPrice))}원</S.Price>
+            </S.MiddleWrap>
+            <S.LeftWrap>
+              <S.Quantity>
+                <S.QuantityBtn
+                  onClick={() => {
+                    props.onClickQuantityBtn(index, "minus");
+                  }}
+                >
+                  -
+                </S.QuantityBtn>
+                <S.Input
+                  value={Number(el.quantity)}
+                  maxLength={3}
+                  onChange={(event) => {
+                    props.onChangeInput(event, index);
+                  }}
+                />
+                <S.QuantityBtn
+                  onClick={() => {
+                    props.onClickQuantityBtn(index, "plus");
+                  }}
+                >
+                  +
+                </S.QuantityBtn>
+              </S.Quantity>
+            </S.LeftWrap>
+          </S.ItemWrap>
         ))}
       <S.Label>합계: {}</S.Label>
     </div>
